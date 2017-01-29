28fac3d1f5557c97515f9d0113dd0be42eabda05
Surasak La-ongkham <surasak.ict.msu@gmail.com>
2017-01-29 15:51:08 +0700

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -55,17 +56,23 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); // Use _.pick to only pick description and completed
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
+	});
 
-	body.description = body.description.trim();
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	body.id = todoNextId++;
+	// body.description = body.description.trim();
 
-	todos.push(body);
+	// body.id = todoNextId++;
 
-	res.json(body);
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -115,6 +122,8 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
